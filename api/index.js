2e2b5797e3b3a50eb6a7917b917650df428f2f47
gimhaninavodya <rechalgim@gmail.com>
2024-01-27 20:04:28 +0530

    complete sign up page functionality

--- api/index.js
+++ api/index.js
@@ -3,9 +3,12 @@ import mongoose from "mongoose";
 import dotenv from "dotenv";
 import userRouter from './routes/user.route.js';
 import authRouter from './routes/auth.route.js';
+import cors from "cors";
 dotenv.config();
 
-mongoose.connect(process.env.MONGO).then(() => {
+mongoose
+    .connect(process.env.MONGO)
+    .then(() => {
     console.log('connected to mongoDB');
     }).catch((err) => {
         console.log(err);
@@ -14,6 +17,7 @@ mongoose.connect(process.env.MONGO).then(() => {
 const app = express();
 
 app.use(express.json());
+app.use(cors());
 
 app.listen( 3000, () =>{
     console.log('Server is running on port 3000!');
@@ -23,6 +27,7 @@ app.listen( 3000, () =>{
 app.use("/api/user",userRouter);
 app.use("/api/auth", authRouter);
 
+//middleware error handling
 app.use((err, req, res, next) => {
     const statusCode = err.statusCode || 500;
     const message = err.message || 'Internal server error';
--- client/vite.config.js
+++ client/vite.config.js
@@ -3,13 +3,5 @@ import react from '@vitejs/plugin-react-swc';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  server:{
-    Proxy: {
-      '/api': {
-        target:'http://localhost:3000',
-        secure: false,
-      },
-    },
-  },
   plugins: [react()],
 });
